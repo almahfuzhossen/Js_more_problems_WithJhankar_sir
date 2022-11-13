0b4cafbe5d1823bcc803399a30848d241d4f1e51
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-13 15:19:26 +0600

    input error

---
+++ input-error.js
@@ -0,0 +1,19 @@
+function add (num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+const firstTotal = add ( 44, 55);
+// console.log(firstTotal);
+const secondTotal = add ( 776, 876);
+// console.log(secondTotal);
+const thirdTotal = add ( 5654, 665);
+console.log(thirdTotal);
+
+function multiply(num1, num2){
+    console.log('parameter ', num1, num2);
+    const result = num1 * num2;
+    return result;
+}
+
+const firstResult = multiply( 34, 66);
+console.log(firstResult);
