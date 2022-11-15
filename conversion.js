0f038307710e7719b95a70383b821173c245660e
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-15 22:48:46 +0600

    parseInt and parseFloat added

--- conversion.js
+++ conversion.js
@@ -20,4 +20,12 @@ var maltaPrice = '43.44';
 var aluPriceNumber = parseInt(aluPrice);
 var maltaPriceNumber = parseInt(maltaPrice);
 // console.log(aluPriceNumber + maltaPriceNumber);
-// --------------------------------------
+// -------------------------------------
+
+var aluPrice = '33.99';
+var maltaPrice = '33.87';
+var aluPriceNumber = parseFloat(aluPrice);
+var maltaPriceNumber = parseFloat(maltaPrice);
+console.log( aluPriceNumber + maltaPriceNumber);
+
+// ----------------------------------------
