af71ac8fa8938ab36c76c6920c6fa02d0b68400e
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-15 16:53:21 +0600

    math oparation

--- mathOparation.js
+++ mathOparation.js
@@ -2,3 +2,9 @@ var onionPrice = 45;
 var eggPrice = 43;
  var totalPrice = onionPrice + eggPrice;
  console.log(totalPrice);
+
+//  --------------------------------
+ var phonePrice = 34555;
+ var iceCreamPrice = 566;
+  var priceDifference =  phonePrice - iceCreamPrice;
+  console.log(priceDifference);
