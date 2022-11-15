2ec9d6aad6b723b929794955c1745b67e6471560
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-15 21:53:13 +0600

    two string has added

---
+++ conversion.js
@@ -0,0 +1,9 @@
+var firstName = 'kim';
+var lastName = 'jannie';
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+// -----------------------
+var firstName = 'Tom';
+var lastName = 'Cruise';
+var fullName = firstName + ' ' + lastName;
+// console.log(fullName);
