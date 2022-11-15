9166cbfd2eeb91833c5d0ae1f6cf3173f7d62071
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-15 22:30:36 +0600

    conversion

--- conversion.js
+++ conversion.js
@@ -7,3 +7,12 @@ var firstName = 'Tom';
 var lastName = 'Cruise';
 var fullName = firstName + ' ' + lastName;
 // console.log(fullName);
+// ------------------------
+var onionPrice = '43.66';
+var eggPrice = '23.40';
+// var onionPriceNumber = parseInt(onionPrice);
+// var onionPriceNumber = parseFloat(onionPrice);
+// console.log(onionPriceNumber);
+// -------------------------
+
+
