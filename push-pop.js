04811ff889a89ee8f948ee8259c03bba5bfeb273
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-16 15:18:01 +0600

    push pop in details

--- push-pop.js
+++ push-pop.js
@@ -10,5 +10,15 @@ var friendAge =[ 33, 44, 555, 66, 23, 456, 5];
 friendAge.push(21);
 friendAge.push(43);
 friendAge.push(27);
-console.log(friendAge);
+// console.log(friendAge);
 
+// remove item from array
+
+var friendAge =[ 33, 44, 555, 66, 23, 456, 5];
+friendAge.pop();
+friendAge.pop();git
+// console.log(friendAge);
+
+var bookName = ['old', 'marriage', 'titanic', 'revere', 'short'];
+var lastBook = bookName.pop();
+console.log(lastBook);
