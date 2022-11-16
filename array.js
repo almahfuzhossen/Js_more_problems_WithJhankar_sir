fb816975754e754581bae755355a7b50e4c06317
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-16 14:17:28 +0600

    IndexOf and How to change index element

--- array.js
+++ array.js
@@ -9,3 +9,23 @@ var vowels = [ 'a', 'e', 'i', 'o', 'u'];
 // console.log(vowels.length);
 var vowelsCount = vowels.length
 console.log (vowelsCount);
+
+// ------ array index----------//
+
+var bookPages =[ 'Good', 'Bad', 'Better', 'Best', 'Ill'];
+var betterIndex = bookPages.indexOf('Better');
+// console.log(betterIndex);
+
+var numbers =[12, 34, 655, 63, 5, 45, 23, 76];
+// var numberIndex = numbers.length
+// console.log(numberIndex);
+
+// var numberIndex = numbers.indexOf(5);
+// console.log(numberIndex);
+
+// var numberIndex = numbers.indexOf(76);
+// console.log(numberIndex);
+
+var PositionOfBody =[ 'Good', 'Bad', 'Better', 'Best', 'Ill'];
+var thirdPosition =  PositionOfBody[4];
+console.log(thirdPosition);
