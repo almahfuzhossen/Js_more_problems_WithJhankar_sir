314ba477b1d2a36ab955f5c4d14d859d41ee1c26
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-16 20:16:58 +0600

    multiple conditions

--- multipleConditions.js
+++ multipleConditions.js
@@ -1,9 +1,18 @@
 var isGraduated = true;
 var salary = 75000;
-
-if( isGraduated == true){
-    console.log('eso biye kori');
-}
-else{
-    console.log('Tor kopale biya nai');
-}
+var car = 1;
+//--------------------------------
+// if( isGraduated == true){
+//     console.log('eso biye kori');
+// }
+// else{
+//     console.log('Tor kopale biya nai');
+// }
+// ---------------------------------
+// if (isGraduated == true && salary > 50000){
+//     console.log('Eso biye kore feli');
+// }
+// else {
+//     console.log('tor kopale biya nai');
+// }
+//----------------------------------------
