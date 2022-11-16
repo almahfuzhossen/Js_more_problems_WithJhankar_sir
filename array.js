8c02264c7e3e6cbb3e973c53473c3ac8defdfed1
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-16 12:18:44 +0600

    array length revision

---
+++ array.js
@@ -0,0 +1,11 @@
+// -------Array declare-----------//
+
+var friendsAge = [ 22, 23, 54, 675];
+var models = ['Moushumi', 'Shabnoor', 'Bubli'];
+var vowels = [ 'a', 'e', 'i', 'o', 'u'];
+
+// -------- array.length-------------//
+
+// console.log(vowels.length);
+var vowelsCount = vowels.length
+console.log (vowelsCount);
