b365a09518daa4a418bba0da0a5fd5b3d7b4d798
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-16 14:49:42 +0600

    push practice

---
+++ push-pop.js
@@ -0,0 +1,14 @@
+
+// ------------push-----------------
+var lastBench =[ 'leo', 'peo', 'donal', 'style', 'marge' , 'pong'];
+lastBench.push('chip');
+lastBench.push('arnold');
+lastBench.push('architect');
+// console.log(lastBench);
+
+var friendAge =[ 33, 44, 555, 66, 23, 456, 5];
+friendAge.push(21);
+friendAge.push(43);
+friendAge.push(27);
+console.log(friendAge);
+
