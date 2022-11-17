967dd8be3c6dda94c773518797b671b76d78d442
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-17 16:33:52 +0600

    even and odd number while loop

--- whileloop.js
+++ whileloop.js
@@ -20,3 +20,11 @@
 //      number = number + 2;
 //  }
 // -------------------------------
+
+// even numbers...
+// var number = 0;
+// while ( number <= 20){
+//     console.log(number);
+//     number = number + 2;
+// }
+// --------------------------------
