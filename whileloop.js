bc4b4b18f484d2571d8b091745fa85cdc9e4b98b
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-17 14:28:22 +0600

    while loop practice

---
+++ whileloop.js
@@ -0,0 +1,7 @@
+// var roastGiven = 0;
+// while ( roastGiven < 10){
+//     console.log('Roast den, please');
+//     roastGiven ++;
+    // console.log(roastGiven);
+// }
+// ---------------------------
