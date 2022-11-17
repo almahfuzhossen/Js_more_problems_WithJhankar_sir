ead3d874174e4bd1aac419af8aed25d9ab480469
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-17 15:55:25 +0600

    whileloop more

--- whileloop.js
+++ whileloop.js
@@ -5,3 +5,10 @@
     // console.log(roastGiven);
 // }
 // ---------------------------
+
+// var numbers = 0;
+// while ( numbers <= 10) {
+//     console.log(numbers);
+//     numbers++;
+// }
+// --------------------------
