c1afbd70e3d1e80d5c189a489839b4f803a733d5
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-18 15:34:49 +0600

    multiple finction

--- function.js
+++ function.js
@@ -1,10 +1,24 @@
- function bringPizza (taka){
-     console.log('pizza r jonne dise', taka);
-     console.log('mama pizza den');
-     var pizzaPrice = 50;
-     pizzaQuantity = taka / pizzaPrice;
-     return pizzaQuantity;
- }
- var money = 500;
- var pizza = bringPizza(money);
- console.log('ei nen pizza', pizza);
+//  function bringPizza (taka){
+//      console.log('pizza r jonne dise', taka);
+//      console.log('mama pizza den');
+//      var pizzaPrice = 50;
+//      pizzaQuantity = taka / pizzaPrice;
+//      return pizzaQuantity;
+//  }
+//  var money = 500;
+//  var pizza = bringPizza(money);
+//  console.log('ei nen pizza', pizza);
+
+// ------------------------------------
+
+// function addTwoNumbers( num1, num2){
+//     console.log(num1 , num2);
+//     var total = num1 + num2;
+//     return total;
+// }
+
+// var firstNumber = 23;
+// var secondNumber = 45;
+// var result = addTwoNumbers(firstNumber, secondNumber);
+// console.log('value of two numbers', result);
+// ----------------------------------------
