8fcf687375fcec387c78a9ed5e8dab12681cd806
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-18 21:04:30 +0600

    switch and default

---
+++ switch.js
@@ -0,0 +1,40 @@
+const color = 'yellow';
+if (color == 'green'){
+    console.log('You are my green friend');
+} 
+else if (color == 'red'){
+    console.log('you are my red friend');
+}
+else if (color == 'white'){
+    console.log('you are my white friend');
+}
+else if ( color == 'pink'){
+    console.log('you are my pink');
+}
+else if (color == 'blue'){
+    console.log('you are my blue brother');
+}
+else if ( color == 'black'){
+    console.log('you are my black brother');
+}
+else
+    console.log('you are my yellow baby');
+    
+// switch
+switch (color){
+    case 'green':
+        console.log('you are my green');
+        break;
+    case 'blue':
+        console.log('you are my blue');
+        break;
+    case 'pink':
+        console.log('you are my pink'); 
+        break;
+    case 'white':
+        console.log('you are my white');
+        break;
+    default:
+        console.log('you are my lovely yellow');
+
+}
