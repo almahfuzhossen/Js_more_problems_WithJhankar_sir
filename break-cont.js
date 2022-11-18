e9e37536f014f6de7d6cb87aeb60a88dc364b44c
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-19 01:42:16 +0600

    break cont added

---
+++ break-cont.js
@@ -0,0 +1,28 @@
+// var i= 0;
+// while ( i < 10){
+//     console.log(i);
+//     if ( i == 5){
+//         break;
+//     }
+//     i++;
+// }
+// -----------------
+
+// for ( var i=0; i < 20; i++){
+//     if ( i == 10){
+//         break;
+//     }
+//     console.log(i);
+// }
+
+// ------------------
+
+// var numbers = [23, 45, 64, 34, 56, 46, 66, 54, 67, 70, 80];
+// for (var i = 0; i< numbers.length; i++){
+//     var number =numbers[i];
+//     console.log(number);
+//     if( number > 70){
+//         break;
+//     }
+// }
+// ---------------------
