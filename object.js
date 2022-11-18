934b3a2f5a9e9efa625290e0075910e55aaaff66
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-18 18:41:57 +0600

    object

---
+++ object.js
@@ -0,0 +1,37 @@
+var student = { 
+    id: 115, 
+    name: 'Tom Khan', 
+    class: 9, 
+    marks: 98
+};
+
+var mobile = {
+    brand: 'Samsung',
+    price: 19000, 
+    storage: '64gb',
+    camera: '7MP'
+};
+
+
+var myComputer = {
+    brand: 'lonovo',
+    price: 39000,
+    color: 'silver',
+    processor: 'i7',
+    storage: '500gb'
+};
+// ------------------------
+// console.log(mobile);
+// console.log(myComputer.processor);
+// console.log(student.id);
+// ---------------------------
+
+// var computerPrice = myComputer.storage;
+// console.log(computerPrice);
+// ----
+// var mobileProperty = mobile.price;
+// console.log(mobileProperty);
+// ----
+// var studentProperty = student.name;
+// console.log(studentProperty);
+// ----
