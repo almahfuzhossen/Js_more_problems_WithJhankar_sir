75cbb126b5f748f6c05a71b503bc684980d3a0f7
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-19 14:38:33 +0600

    inches to feet

---
+++ inchtofeet.js
@@ -0,0 +1,22 @@
+function inchToFeet(inches){
+var feet = inches / 12;
+return feet;
+}
+
+
+
+var myInches = 132;
+var feet = inchToFeet(myInches);
+console.log('my inches to feet', feet);
+
+var dadaInches = 142;
+var feet = inchToFeet(dadaInches);
+console.log('dada inches to feet', feet);
+
+var dadiInches = 152;
+var feet = inchToFeet(dadiInches);
+console.log('dadi inches to feet', feet);
+
+var nanaInches = 162;
+var feet = inchToFeet(nanaInches);
+console.log('nana inches to feet', feet);
