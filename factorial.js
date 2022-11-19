23fc489140444979cd2cfebb301e3622d0ab4055
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-19 19:31:12 +0600

    function with factorial

--- factorial.js
+++ factorial.js
@@ -7,3 +7,22 @@ for (var i = 1; i <= 7; i++){
 console.log(factorial);
 
 // --------------
+//----- factorial function----------
+
+function GetFactorial (number){
+  let factorial = 1;
+  for( var i = 1; i <= number; i++){
+      factorial = factorial * i;
+  }
+  return factorial;
+}
+
+var myFirstNum = 8;
+var Result = GetFactorial(myFirstNum);
+console.log('Our factorial Result Is ', Result)
+// ------------
+
+var mySecondNum = 5;
+var Result = GetFactorial(mySecondNum);
+console.log('our best factorial result is ', Result)
+// ------------
