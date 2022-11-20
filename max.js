74350d7a72144fa62947a81fd6626871038870c6
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-20 21:21:43 +0600

    sum calculation in an array

--- max.js
+++ max.js
@@ -1,6 +1,6 @@
-var business = 350;
-var minister = 450;
-var army = 700;
+// var business = 350;
+// var minister = 450;
+// var army = 700;
 
 // if( business > minister){
 //     console.log('businees man er pola is bigger')
@@ -9,12 +9,26 @@ var army = 700;
 //     console.log('minister er pola is bigger')
 // -------------------------
 
-function findLargest( first , second){
-      if ( first > second){
-          return true;
-      }
-      return false;
-}
+// function findLargest( first , second){
+//       if ( first > second){
+//           return true;
+//       }
+//       return false;
+// }
 
-var myNumber = findLargest ( 23 , 56);
-console.log('the number is biggest than others ', myNumber)
+// var myNumber = findLargest ( 23 , 56);
+// console.log('the number is biggest than others ', myNumber)
+
+// ------ sum calculation in array---------
+
+function ArrayTotal (numbers){
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++){
+        const element = numbers[i];
+        sum = sum + element;
+   }
+     return sum;
+ }
+   
+    var total = ArrayTotal([34, 565, 35]);
+    console.log('sum of an array total ', total)
