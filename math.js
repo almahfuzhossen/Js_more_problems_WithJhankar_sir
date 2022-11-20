85160920ccb32c7909c356e288efb689aa709955
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-20 14:46:29 +0600

    math oparation

--- math.js
+++ math.js
@@ -1,9 +1,25 @@
 const myNumber = -13;
-const output = Math.abs(myNumber);
-console.log(output);
+// const output = Math.abs(myNumber);
+// console.log(output);
 
 // --------------
 let OurNumber = 13.4567;
-let Output = Math.ceil(OurNumber);
-console.log(Output)
-// ----------------g
+// let Output = Math.ceil(OurNumber);
+// console.log(Output)
+// ----------------
+
+let HerNumber = 24.5677;
+// var Output = Math.floor(HerNumber);
+// console.log(Output);
+
+// ----------------
+ let Number = 34.7658;
+//  let Output =Math.round(Number);
+//  console.log(Output)
+
+//  ----------------
+
+const prettyNumber = 23;
+const output = Math.random(prettyNumber) * 10;
+const rounded = Math.floor(output);
+console.log(rounded)
