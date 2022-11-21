f2f618a002b91efe38072c88b037b72a189eca13
al-mahfuz <almahfuzhossen@gmail.com>
2022-11-21 23:08:25 +0600

    cheapest phone

--- duplicateReverse.js
+++ duplicateReverse.js
@@ -78,4 +78,22 @@
 
 // }
 
+// ----- lowest phone in an array --------
+
+// const phones = [
+//     { name: 'samsung s5', price: 45000, camera: 10, storage: 32 },
+//     { name: 'walton m32', price: 15000, camera: 8, storage: 8 },
+//     { name: 'shaomi m3', price: 12000, camera: 8, storage: 16 },
+//     { name: 'oppo a2', price: 17000, camera: 8, storage: 32 },
+//     { name: 'nokia n95', price: 8000, camera: 8, storage: 4 },
+//     { name: 'htc h81', price: 25000, camera: 8, storage: 16 }
+// ];
+
+// let cheapest = phones[0];
+// for ( const phone of phones){
+//     if(phone.price < phone.cheapest){
+//         cheapest = phone;
+//     }
+// }
+// console.log(cheapest);
 
